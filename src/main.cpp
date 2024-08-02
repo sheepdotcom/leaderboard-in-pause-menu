452c8cdb85ac8391114c13f07164741403274872
sheepdotcom <magicsheepwizard3@gmail.com>
2024-08-02 15:16:18 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/PauseLayer.hpp>
+class $modify(PauseLayer) {
+	void customSetup() {
+		PauseLayer::customSetup();
+	}
+};
