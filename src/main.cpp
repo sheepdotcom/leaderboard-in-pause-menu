3edfd7fe97860a4f2f9fc1f381552b073ef960d7
sheepdotcom <magicsheepwizard3@gmail.com>
2024-08-04 11:05:14 -0400

    rejected because no updateLayout :guh:

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 using namespace geode::prelude;
 
+//I'm gonna leave comments in here because I like to
+//At least this code doesn't have any catgirls in it unlike my other project :3
 #include <Geode/modify/PauseLayer.hpp>
 class $modify(LDPauseLayer, PauseLayer) {
 	void customSetup() {
@@ -10,12 +12,13 @@ class $modify(LDPauseLayer, PauseLayer) {
 		auto spr = CCSprite::createWithSpriteFrameName("GJ_levelLeaderboardBtn_001.png");
 		spr->setScale(0.65f);
 		auto btn = CCMenuItemSpriteExtra::create(spr, this, menu_selector(LDPauseLayer::onLeaderboard));
-		btn->setID("leaderboard-button");
 		rightButtonMenu->addChild(btn);
+		btn->setID("leaderboard-button"_spr);
+		rightButtonMenu->updateLayout();
 	}
 	
 	void onLeaderboard(CCObject* sender) {
-		if (PlayLayer::get()) {
+		if (PlayLayer::get()) { //Check if there is a PlayLayer
 			auto level = PlayLayer::get()->m_level;
 			auto levelLeaderboardType = static_cast<LevelLeaderboardType>(GameManager::get()->getIntGameVariable("0098"));
 			auto levelLeaderboardMode = static_cast<LevelLeaderboardMode>(GameManager::get()->getIntGameVariable("0164"));
@@ -31,7 +34,7 @@ class $modify(LDLevelLeaderboard, LevelLeaderboard) {
 	bool init(GJGameLevel* p0, LevelLeaderboardType p1, LevelLeaderboardMode p2) {
 		if (!LevelLeaderboard::init(p0, p1, p2)) return false;
 
-		//Makes sure its above everything if its created without the show function
+		//Makes sure its above everything if its created without the show function (I'm looking at you betterinfo)
 		this->setZOrder(std::max(0x69, CCDirector::sharedDirector()->getRunningScene()->getHighestChildZ()));
 
 		return true;
